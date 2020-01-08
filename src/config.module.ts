b493798892596abf56b4f51df5848cf7fcefc774
krivochenko <admin@krivochenko.ru>
2020-01-08 03:36:32 +0300

    Fix process of building config

--- src/config.module.ts
+++ src/config.module.ts
@@ -29,6 +29,7 @@ export class ConfigModule {
                     key,
                   );
                   previous[key] = func();
+                  return previous;
                 });
             }, {});
           },
